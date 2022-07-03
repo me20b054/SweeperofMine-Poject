7b0d79797b187d57ca02aa402167f3dd58477dd0
Naveen 2605 <87110834+me20b054@users.noreply.github.com>
2022-07-03 17:51:42 +0530

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,65 @@
+class Board():
+    def __init__(self,dim_size,num_bombs):
+        self.dim_size = dim_size 
+        self.num_bombs = num_bombs 
+
+
+        self.board = self.make_new_board() 
+        self.assign_values_to_board()  
+        self.dug = set()
+
+
+        def make_new_board(self): 
+            board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)] 
+
+            bombs_planted = 0  
+            while bombs_planted < self.num_bombs:
+                loc = random.randint(0,self.dim_size**2 - 1)
+                row = loc // self.dim_size
+                col = loc % self.dim_size 
+
+                if board[row][col] == "*": 
+                    continue 
+                board[row][col] = '*'   # planting bombs 
+                bombs_planted += 1  
+
+            return board 
+    
+        def assign_values_to_board(self): 
+            for r in range(self.dim_size):
+                for c in range(slef.dim_size):
+                    if board[r][c] == "*":
+                        continue 
+                    self.board[r][c] == self.num_neighbour_bombs
+
+        def num_neighbour_bombs(self,row,col): 
+            num_neighbour_bombs = 0
+            for r in range(max(0,row-1),min(self.dim_size -1, (row+1)+1)): 
+                for c in range(max(0,col-1),min(self.dim_size -1 ,(col+1)+1)): 
+                    if r == row and c == col : 
+                        continue
+                    if self.board[r][c] == '*':
+                        num_neighbour_bombs += 1  
+
+        def dig(self, row, col): 
+            self.dug,add((row,col)) 
+            if self.board[row][col] == '*':
+                return False 
+            elif self.board[row][col] > 0:
+                return True 
+            for r in range(max(0,row-1),min(self.dim_size -1, (row+1)+1)): 
+                for c in range(max(0,col-1),min(self.dim_size -1 ,(col+1)+1)):
+                    if (r, c) in self.dug:
+                        continue
+                    self.dig(r, c)
+
+            return True 
+            
+            
+
 def play(dim_size=10, num_bombs=10):
-    # Step 1: create the board and plant the bombs
     board = Board(dim_size, num_bombs)
 
-    # Step 2: show the user the board and ask for where they want to dig
-    # Step 3a: if location is a bomb, show game over message
-    # Step 3b: if location is not a bomb, dig recursively until each square is at least
-    #          next to a bomb
-    # Step 4: repeat steps 2 and 3a/b until there are no more places to dig -> VICTORY!
     safe = True 
 
     while len(board.dug) < board.dim_size ** 2 - num_bombs:
@@ -17,14 +70,10 @@ def play(dim_size=10, num_bombs=10):
         if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
             print("Invalid location. Try again.")
             continue
-
-        # if it's valid, we dig
         safe = board.dig(row, col)
         if not safe:
-            # dug a bomb ahhhhhhh
-            break # (game over rip)
+            break
 
-    # 2 ways to end loop, lets check which one
     if safe:
         print("CONGRATULATIONS!!!! YOU ARE VICTORIOUS!")
     else:
@@ -32,6 +81,8 @@ def play(dim_size=10, num_bombs=10):
         # let's reveal the whole board!
         board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
         print(board)
+        
 
 if __name__ == '__main__': # good practice :)
     play()
+    
